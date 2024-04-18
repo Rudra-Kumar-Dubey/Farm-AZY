4931df934a9735e2a3e6239b9f998656070e7b11
Rudra Kumar Dubey <rudradubey901@gmail.com>
2024-04-18 15:26:05 +0530

    ENV CHANGES

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,12 +7,19 @@ import "firebase/compat/storage";
 // dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVXicAfJKAJDBADaIvbVlt4LRDb1R9SXk",
-  authDomain: "farmeazy-8941d.firebaseapp.com",
-  projectId: "farmeazy-8941d",
-  storageBucket: "farmeazy-8941d.appspot.com",
-  messagingSenderId: "750069966622",
-  appId: "1:750069966622:web:d33d7431c5c32f8ad7cb0f"
+  // apiKey: "AIzaSyCVXicAfJKAJDBADaIvbVlt4LRDb1R9SXk",
+  // authDomain: "farmeazy-8941d.firebaseapp.com",
+  // projectId: "farmeazy-8941d",
+  // storageBucket: "farmeazy-8941d.appspot.com",
+  // messagingSenderId: "750069966622",
+  // appId: "1:750069966622:web:d33d7431c5c32f8ad7cb0f"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+
 };
 
 export const Firebase = firebase.initializeApp(firebaseConfig);
